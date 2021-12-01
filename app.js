433f841b60d3597626a1fe37d6d0b2bc8e189515
Dariusz Okonski <darek200180@gmail.com>
2021-12-01 02:41:51 +0000

    add js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const cards = document.querySelectorAll('.card');
+
+console.log(cards)
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+
+        clearActive();
+
+        card.classList.add('active');
+
+    })
+});
+
+function clearActive() {
+    cards.forEach(card => {
+        if(card.classList.contains('active'))
+            card.classList.remove('active');
+    })
+}
